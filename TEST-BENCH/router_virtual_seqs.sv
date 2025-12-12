2b1ccd3f90d5a7931ede3a3263ab94363c578e90
Abhishek chougala <abhishekchougala9@gmail.com>
2025-12-12 13:49:40 +0530

    Create router_virtual_seqs.sv

---
+++ TEST-BENCH/router_virtual_seqs.sv
@@ -0,0 +1,10 @@
+
+
+class router_virtual_seqs extends uvm_sequence #(uvm_sequence_item);
+	`uvm_object_utils(router_virtual_seqs)  
+	
+	function new(string name ="router_virtual_seqs");
+		super.new(name);
+	endfunction
+endclass
+
